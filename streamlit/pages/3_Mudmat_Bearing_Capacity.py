e8782f063cf714f7706c78974ef9607188fcb68c
J.S <91623332+jrson11@users.noreply.github.com>
2024-03-25 21:56:59 -0500

    Update 3_Mudmat_Bearing_Capacity.py

--- streamlit/pages/3_Mudmat_Bearing_Capacity.py
+++ streamlit/pages/3_Mudmat_Bearing_Capacity.py
@@ -361,10 +361,12 @@ with col_A:
 
 
 with col_B:
+        col1,col2,col3 = st.columns(3)
+        
         with col1:
-            toggle_ULT = st.toggle('Ultimate')
+            toggle_ULT = st.toggle('Ultimate Capacity')
         with col2:
-            toggle_WSD = st.toggle('Allowable')
+            toggle_WSD = st.toggle('Allowable Loads')
         with col3:
             toggle_LRFD = st.toggle('LRFD')
         
