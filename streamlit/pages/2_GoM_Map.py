0a508d57814b42fd4e3c7b38278406b002428658
J.S <91623332+jrson11@users.noreply.github.com>
2024-03-25 21:29:40 -0500

    Update 2_GoM_Map.py

--- streamlit/pages/2_GoM_Map.py
+++ streamlit/pages/2_GoM_Map.py
@@ -1,4 +1,8 @@
 import streamlit as st
+# ====================================================================
+## 셋업
 
-password = st.sidebar.text_input("Please enter the :red[PASSWORD]") # 비번 확인
+st.set_page_config(layout="wide") # 페이지 설정을 wide 모드로 설정
+st.header('GeoSohn - GoM Map') # 타이틀
+password = st.text_input("Please enter the :red[PASSWORD]") # 비번 확인
 
