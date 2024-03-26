a1c13528924c1503a614c26226ba7f3252f83cd2
J.S <91623332+jrson11@users.noreply.github.com>
2024-03-25 22:06:23 -0500

    Update 3_Mudmat_Bearing_Capacity.py

--- streamlit/pages/3_Mudmat_Bearing_Capacity.py
+++ streamlit/pages/3_Mudmat_Bearing_Capacity.py
@@ -430,5 +430,5 @@ with col_B:
             toggle_LRFD = st.toggle('LRFD')
 
     with tab3:
-        pass
+        st.image('https://github.com/jrson11/GeoSohn_app/blob/main/streamlit/images/Mudmat_bearing_capacity-Diagram.jpg')
 
